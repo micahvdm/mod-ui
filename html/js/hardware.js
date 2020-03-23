963d6ba65e516137ffa15e4e7c7a9e3fcd47f91c
Giedrius <giedrius@blokas.io>
2020-03-23 15:16:35 +0000

    Fixed MIDI learn

--- html/js/hardware.js
+++ html/js/hardware.js
@@ -791,8 +791,14 @@ function HardwareManager(options) {
 
         var actuators = self.availableActuators(instance, port, currentAddressing.tempo)
         var typeOptions = [kNullAddressURI, deviceOption, kMidiLearnURI, ccOption, cvOption]
+        var hiddenOptions = [deviceOption, ccOption]
         var i = 0
         typeSelect.find('option').unwrap().each(function() {
+            if (hiddenOptions.includes(typeOptions[i])) {
+              $(this).remove()
+              i++
+              return
+            }
             var btn = $('<div class="btn js-type" data-value="'+typeOptions[i]+'">'+$(this).text()+'</div>');
             var jbtn = $(btn);
             if(jbtn.attr('data-value') == typeInput.val()) {
@@ -805,6 +811,7 @@ function HardwareManager(options) {
             // Hide MIDI tab if not available
             else if (jbtn.attr('data-value') === kMidiLearnURI && !actuators[kMidiLearnURI]) {
               jbtn.hide()
+              form.find('.midi-no-mapping-possible').show()
             }
             // Hide CV tab if not available
             else if (jbtn.attr('data-value') === cvOption && !self.isCvAvailable(port)) {
